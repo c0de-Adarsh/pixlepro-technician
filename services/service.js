fb506972b92b578575901bea1c265864001db41c
Vaibhav <vaibhav@example.com>
2026-08-26 20:59:14 +0530

    live

--- services/service.js
+++ services/service.js
@@ -1,5 +1,9 @@
 import axios from "axios";
-const ConstantsUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3009/";
+const ConstantsUrl = process.env.NEXT_PUBLIC_API_URL || "https://pixle-technician.onrender.com/";
+
+
+// const Constants = "https://pixle-technician.onrender.com/"
+// const ConstantsUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3009/";
 
 let cachedApiKey = "";
 
